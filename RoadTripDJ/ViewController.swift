2bf8b7934910bf3b676f4c1e7a677b6a0d81f6fc
Graeme Mathieson <mathie@woss.name>
2015-08-23 08:48:11 +0100

    Show a media picker when 'add' is hit.

--- RoadTripDJ/ViewController.swift
+++ RoadTripDJ/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
@@ -15,11 +16,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addMusic(sender: UIBarButtonItem) {
+        let mediaPicker = MPMediaPickerController(mediaTypes: .Music)
+        presentViewController(mediaPicker, animated: true, completion: nil)
     }
-
-
 }
 
