cb22b9ea556a0252b98d9aeb3c8fdf26eb3fabc9
Graeme Mathieson <mathie@woss.name>
2015-08-24 07:47:51 +0100

    Implement the play/pause button.

--- RoadTripDJ/ViewController.swift
+++ RoadTripDJ/ViewController.swift
@@ -10,13 +10,52 @@ import UIKit
 import MediaPlayer
 
 class ViewController: UIViewController {
+    @IBOutlet weak var playerBar: UIToolbar!
+    var playButton: UIBarButtonItem!
 
-    var playlist: MPMediaItemCollection = MPMediaItemCollection(items: [])
+    var playlist = MPMediaItemCollection(items: [])
+    let player = MPMusicPlayerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    private func togglePlayPause() {
+        switch(player.playbackState) {
+        case .Playing:
+            player.pause()
+        default:
+            player.play()
+        }
+        
+        setPlayButtonForPlaybackState(player.playbackState)
+    }
+
+    private func playButtonItemForPlaybackState(state: MPMusicPlaybackState) -> UIBarButtonItem {
+        let systemItem: UIBarButtonSystemItem
+        switch(state) {
+        case .Playing:
+            systemItem = .Pause
+        default:
+            systemItem = .Play
+        }
+        
+        return UIBarButtonItem(barButtonSystemItem: systemItem, target: self, action: Selector("playPause:"))
+    }
+
+    private func setPlayButtonForPlaybackState(state: MPMusicPlaybackState) {
+        playerBar.items = playerBar.items?.map { (barButtonItem: UIBarButtonItem) -> UIBarButtonItem in
+            if(barButtonItem == playButton) {
+                playButton = playButtonItemForPlaybackState(state)
+                return playButton
+            } else {
+                return barButtonItem
+            }
+        }
+    }
+
+    // MARK: Actions
 
     @IBAction func addMusic(sender: UIBarButtonItem) {
         let mediaPicker = MPMediaPickerController(mediaTypes: .Music)
@@ -28,12 +67,18 @@ class ViewController: UIViewController {
         presentViewController(mediaPicker, animated: true, completion: nil)
     }
 
+    @IBAction func playPause(sender: UIBarButtonItem) {
+        playButton = sender // FIXME: Somewhat subtopimal.
+        togglePlayPause()
+    }
+
 }
 
+// MARK: MPMediaPickerControllerDelegate
 extension ViewController : MPMediaPickerControllerDelegate {
     func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
         print("mediaPicker:\(mediaPicker) didPickMediaItems:\(mediaItemCollection)")
-
+        
         let items = playlist.items + mediaItemCollection.items
         playlist = MPMediaItemCollection(items: items)
         
@@ -42,14 +87,22 @@ extension ViewController : MPMediaPickerControllerDelegate {
             index += 1
             print("\(index): \(item.artist) - \(item.title)")
         }
-
+        
+        player.setQueueWithItemCollection(playlist)
+        
+        switch(player.playbackState) {
+        case .Playing:
+            break
+        default:
+            player.play()
+        }
+        
         dismissViewControllerAnimated(true, completion: nil)
     }
-
+    
     func mediaPickerDidCancel(mediaPicker: MPMediaPickerController) {
         print("mediaPickerDidCancel:\(mediaPicker)")
         
         dismissViewControllerAnimated(true, completion: nil)
     }
 }
-
